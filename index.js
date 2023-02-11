58d4f581e9caafab3b0ccdc44cf657edd21804f9
vaibhav <porwalvaibhav10@gmail.com>
2023-02-11 20:53:22 +0530

    removed delete functionality from index

--- index.js
+++ index.js
@@ -72,12 +72,6 @@ app.put('/products/:id', async (req,res)=>{
     res.redirect(`/products/${product._id}`)
 })
 
-app.delete('/products/:id', async(req,res)=>{
-    const {id} = req.params;
-    await Product.findByIdAndDelete(id);
-    res.redirect('/products')
-   
-})
 
 
 app.listen(3000, ()=>{
